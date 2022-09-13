9f05b25d3b2ec262e213090f9750f375ea8f5704
unknown <choesenbyme@hotmail.com>
2022-09-13 13:55:52 -0400

    adding places view page

--- controllers/places.js
+++ controllers/places.js
@@ -1,7 +1,22 @@
 const router = require('express').Router()
 
 router.get('/', (req,res) => {
-    res.send('GET /places')
+    let places = [{
+        name:'CHUANTIANXIA',
+        city: 'Brooklyn',
+        state:'NY',
+        cuisines:'SICHUAN, Chinese Spicy',
+        pic:'https://chuantianxiany.com/wp-content/uploads/2022/04/slide01.jpg'
+    },
+    {
+        name:'Daxi Sichuan',
+        city: 'Flushing',
+        state:'NY',
+        cuisines:'SICHUAN, Chinese Spicy',
+        pic:'https://s3-media0.fl.yelpcdn.com/bphoto/HrOpVbsmLlSJz0L0HaPg-g/o.jpg'
+    }
+    ]
+    res.render('places/index', {places})
 })
 
 
