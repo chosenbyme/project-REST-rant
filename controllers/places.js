783a061e11dcabe0bbbce71c4784a6f465b742c5
unknown <choesenbyme@hotmail.com>
2022-10-20 17:39:47 -0400

    part7 refixed bugs, need to pass id

--- controllers/places.js
+++ controllers/places.js
@@ -12,7 +12,9 @@ router.get('/new', (req,res) => {
 router.post('/', (req, res) => {
     console.log(req.body)
     // Default image if one is not provided
+    if(!req.body.city){
     req.body.pic = 'http://placekitten.com/400/400'
+    }
     if(!req.body.city){
         req.body.city = 'Anytown'
     }
@@ -23,17 +25,20 @@ router.post('/', (req, res) => {
     res.redirect('/places')
   })  
 
-router.get('/:id/edit', (req,res) => {
-  let id = Number(req.params.id)
-  if(isNaN(id)){
-      res.render('error404')
-  }
-  else if (!places[id]){
-      res.render('error404')
-  }
-      res.render('places/edit', {place:places[id], id})
-  }
-)
+  
+  router.get('/:id/edit', (req, res) => {
+    let id = Number(req.params.id)
+    if (isNaN(id)) {
+        res.render('error404')
+    }
+    else if (!places[id]) {
+        res.render('error404')
+    }
+    else {
+      res.render('places/edit', { place: places[id], id })
+    }
+  })
+
 
 router.get('/:id', (req, res) => {
     let id = Number(req.params.id)
@@ -47,6 +52,33 @@ router.get('/:id', (req, res) => {
         res.render('places/show', { place: places[id], id})
     }
   })
+
+  router.put('/:id', (req, res) => {
+    let id = Number(req.params.id)
+    if (isNaN(id)) {
+        res.render('error404')
+    }
+    else if (!places[id]) {
+        res.render('error404')
+    }
+    else {
+        // Dig into req.body and make sure data is valid
+        if (!req.body.pic) {
+            // Default image if one is not provided
+            req.body.pic = 'http://placekitten.com/400/400'
+        }
+        if (!req.body.city) {
+            req.body.city = 'Anytown'
+        }
+        if (!req.body.state) {
+            req.body.state = 'USA'
+        }
+        // Save the new data into places[id]
+        places[id] = req.body
+        res.redirect(`/places/${id}`)
+    }
+  })
+  
 
   router.delete('/:id',(req,res) =>{
     let id = Number(req.params.id)
@@ -63,5 +95,6 @@ router.get('/:id', (req, res) => {
     }
   )
   
+
 
 module.exports = router
--- views/places/edit.jsx
+++ views/places/edit.jsx
@@ -6,32 +6,32 @@ function edit_form(data) {
     <Def>
         <main>
             <h1>Edit Place</h1>
-            <form method="POST" action={`/places/${data.place.id}?_method=PUT`}>
+            <form method="POST" action={`/places/${data.id}?_method=PUT`}>
                 <div className="form-group">
                     <label htmlFor="name">Place Name</label>
                     <input className="form-control" 
                     id="name" 
                     name="name" 
                     required
-                    value={data.place.name}
+                    defaultValue={data.place.name}
                     />
                 </div>
                 <div className="form-group">
                     <label htmlFor="pic">Place Pciture</label>
-                    <input className="form-control" type="url" id="pic" name="pic"
-                    value={data.place.pic}/>
+                    <input className="form-control" id="pic" name="pic"
+                    defaultValue={data.place.pic}/>
                 </div>
                 <div className="form-group">
                     <label htmlFor="city">City</label>
-                    <input className="form-control" id="city" name="city" value={data.place.city}/>
+                    <input className="form-control" id="city" name="city" defaultValue={data.place.city}/>
                 </div>
                 <div className="form-group">
                     <label htmlFor="state">State</label>
-                    <input className="form-control" id="state" name="state" value={data.place.state}/>
+                    <input className="form-control" id="state" name="state" defaultValue={data.place.state}/>
                 </div>
                 <div className="form-group">
                     <label htmlFor="cuisines">Cuisines</label>
-                    <input className="form-control" id="cuisines" name="cuisines" required value={data.place.cuisines}/>
+                    <input className="form-control" id="cuisines" name="cuisines" required defaultValue={data.place.cuisines}/>
                 </div>
                 <input className="btn btn-primary" type="submit" value="Add Place"/>
             </form> 
